abb6599e3f966bc288e9bb828886d60ac0dba179
Vishal Mishra <mishravishal0912@gmail.com>
2017-11-02 12:44:44 +0530

    Updated Index.js
    * no. of params reduced to get encrypted app string
    * bin2hex method removed

--- index.js
+++ index.js
@@ -8,37 +8,18 @@ module.exports = {
 
 function getAuthenticatedUrl(url, queryString, httpRequestType) {
   var updatedTimeStamp = Math.floor(Date.now() / 1000) + appConfig.expiry;
-  if(queryString !== null){
-    var query = queryString + '&accessKey=' + appConfig.appKey + '&timeStamp=' + updatedTimeStamp;
-  }
-  else{
-    var query = '&accessKey=' + appConfig.appKey + '&timeStamp=' + updatedTimeStamp;
-  }
-  var stringToSign = httpRequestType + url + '?' + query;
-  var encryptedAppSecret = getEncryptedAppSecret(stringToSign, appConfig.appSecret);
-  var authToken = bin2hex(encryptedAppSecret);
+  var stringToSign = url + '?' + '&accessKey=' + appConfig.appKey;
+  var authToken = getEncryptedAppString(stringToSign, appConfig.appSecret);
   if (_.includes(appConfig.domain, 'http') === false && _.includes(appConfig.domain, 'https') === false) {
-    var authenticatedUrl = 'http://' + appConfig.domain + url + '?' + queryString + '&authToken=' + authToken;
+    var authenticatedUrl = 'http://' + appConfig.domain + url + '?' + queryString + '&authToken=' + authToken + '&timeStamp=' + updatedTimeStamp;
   } else {
-    var authenticatedUrl = appConfig.domain + url + '?' + queryString + '&authToken=' + authToken;
+    var authenticatedUrl = appConfig.domain + url + '?' + queryString + '&authToken=' + authToken + '&timeStamp=' + updatedTimeStamp;
   }
   return authenticatedUrl;
 }
 
-function bin2hex(s) {
-  var i, l, n;
-  var o = '';
-  s += '';
-
-  for (i = 0, l = s.length; i < l; i++) {
-    n = s.charCodeAt(i).toString(16);
-    o += n.length < 2 ? '0' + n : n;
-  }
-  return o
+function getEncryptedAppString(stringToSign, appSecret) {
+  var hmac = crypto.createHmac('sha1', appSecret);
+  hmac.update(stringToSign);
+  return hmac.digest('hex');
 }
-
-function getEncryptedAppSecret(stringToSign, appSecret){
-    var hmac = crypto.createHmac('sha1', appSecret);
-    hmac.update(stringToSign);
-    return hmac.digest('binary');
-};
